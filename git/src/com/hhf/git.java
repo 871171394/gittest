0b9008a25cfab5bc169cd1993426ac4b6cdf8ae3
zhangsan <zansan@qq.com>
2019-11-11 20:14:56 +0800

    修改文件

--- git/src/com/hhf/git.java
+++ git/src/com/hhf/git.java
@@ -3,5 +3,11 @@ package com.hhf;
 public class git {
     public static void main(String[] args) {
         System.out.println("啊是打扫打扫的");
+        //hello
+    }
+    public void test(){
+        int i=0;
+        i++;
+        System.out.println(i);
     }
 }
